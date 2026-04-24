ace17d22f818b61f195ffe3628082a678c6d96a2
liangsqrt <liangsqrt@163.com>
2026-04-24 10:30:20 +0800

    feat: 长连接的内存优化

--- inbound/tun/tunnel/wrap_conn.go
+++ inbound/tun/tunnel/wrap_conn.go
@@ -14,6 +14,10 @@ func (w *WrappedConn) Read(p []byte) (int, error) {
 	if len(w.Buf) > w.readOffset {
 		n := copy(p, w.Buf[w.readOffset:])
 		w.readOffset += n
+		if w.readOffset >= len(w.Buf) {
+			w.Buf = nil
+			w.readOffset = 0
+		}
 		return n, nil
 	}
 	return w.Conn.Read(p)
--- processor/tcp/types.go
+++ processor/tcp/types.go
@@ -85,6 +85,10 @@ func (w *WrappedConn) Read(p []byte) (int, error) {
 	if len(w.Buf) > w.readOffset {
 		n := copy(p, w.Buf[w.readOffset:])
 		w.readOffset += n
+		if w.readOffset >= len(w.Buf) {
+			w.Buf = nil
+			w.readOffset = 0
+		}
 		return n, nil
 	}
 	if !w.passThroughLogged {
